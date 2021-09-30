f51c23f40cbdb4e27334fa3ea91198beed1cb1ba
williamyhg1 <90809764+williamyhg1@users.noreply.github.com>
2021-09-30 19:08:51 +1000

    modify comments

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 
 
-
-
-
+    let wonClaim = `It's a tie! No one scores!.`
+    let lostClaim = `You Won! You score 1!.`
+    let tieClaim = `You Lost! Computer scores 1!.`
+   
+   
 
 function game () {
-    
+
     /* Generate computer random hands */ 
 
   function computerPlay() {
@@ -19,76 +21,88 @@ function game () {
     };
     };
     
-    /*if player stands off, win or lose to computer hands each round */
-
-    const wonClaim = "It's a tie! No one scores!"
-    const lostClaim = "You Won! You score 1!"
-    const tieClaim = "You Lost! Computer scores 1!"
+    /* If player stands off, win or lose to computer hands each round */
 
     function playRound(playerSelection, computerSelection){
         let input = playerSelection.toLowerCase();
-    if (input == computerSelection) {
-        return tieClaim 
-    } else if (input == 
-        "rock" && computerSelection == "scissors" || input == "paper" && computerSelection == "rock" || input == "scissors" && computerSelection == "paper") {
-            return wonClaim
-        } else if (input == 
-            "scissors" && computerSelection == "rock" || input == "rock" && computerSelection == "paper" || input == "paper" && computerSelection == "scissors"){
-            return lostClaim;
-        } else {
-            return "Not a qualified hand! so computer scores 1!";
-    };
-     };
+        if (input == computerSelection) {
+            return tieClaim 
+            } else if (input == 
+                "rock" && computerSelection == "scissors" || input == "paper" && computerSelection == "rock" || input == "scissors" && computerSelection == "paper") {
+                    return wonClaim;
+            } else if (input == 
+                "scissors" && computerSelection == "rock" || input == "rock" && computerSelection == "paper" || input == "paper" && computerSelection == "scissors"){
+                    return lostClaim;
+            } else {
+                    return "Not a qualified hand! so computer scores 1!";
+        };
+     
+    };  
 
     /* input from player*/
 
-    const playerChoice = prompt("what's your hand?")  
-    const play = playRound(playerChoice, computerPlay());
-    
-    alert(play);
-    
-    /* calculate scores*/
+    const playerChoice = prompt("Rock, Paper or Scissors?") 
+    let gameResult = playRound(playerChoice, computerPlay());
 
+    /* Calculate scores after each round of game*/
+    
     function calcScores(result){
-      if (result == wonClaim){
-        return playerScores ++;
-     } else if (result == lostClaim){
-        return computerScores ++;
-     } else if (result == tieClaim){
-        return playerScores += 0;
-     } else {
-         return computerScores ++;
-     };
+        if (result == wonClaim){
+             return playerScores ++;
+        } else if (result == lostClaim){
+             return computerScores ++;
+        } else if (result == tieClaim){
+             return playerScores + 0;
+        } else {
+            return computerScores ++;
+        };
+
     };
-   return calcScores(play);
-};
+
+    calcScores(gameResult);  
+
+
+return gameResult;
+
+};  
+
+
+/* Scores will be updated after each round of game*/
 
 let playerScores = 0;
-let computerScores = 0;
+let computerScores = 0;    
 
-/*Calculate final scores after all hands*/
+    
+/* Calculate final scores after all rounds*/
 
 function finalScores(){
     if (playerScores > computerScores) {
-        alert(`Your score ${playerScores}, computer scores ${computerScores}, you won! :)`)
-       } else if ( playerScores < computerScores ) {
-           alert(`Your score ${playerScores}, computer scores ${computerScores}, you lost! :(`)} else { 
-           alert (`Your score ${playerScores}, computer scores ${computerScores}, it's a tie! :|`)}
-}
- 
+            alert(`You score ${playerScores}, computer scores ${computerScores}, you won! :)`)
+    } else if ( playerScores < computerScores ) {
+           alert(`You score ${playerScores}, computer scores ${computerScores}, you lost! :(`)
+    } else { 
+           alert (`You score ${playerScores}, computer scores ${computerScores}, it's a tie! :|`)}
+
+ };
+
+
 /* Play 5 rounds with computer */
 
-function play5Rounds(){
-    alert("Let's play 5 rounds of Rock-Paper-Scissors with computer!")
+function playFiveRounds(){
+    alert("Let's play 5 rounds of Rock-Paper-Scissors with computer!");
     for (let i = 0; i < 5; i++){
-    game();
+    alert(game());
   }
-finalScores();
-}
+    finalScores(); 
+};
 
 
 
-play5Rounds();
+playFiveRounds();
+
+
+
+
 
 
 
